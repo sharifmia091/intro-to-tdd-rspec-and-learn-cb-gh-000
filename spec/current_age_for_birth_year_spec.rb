0e463846124790720b4e5023da32029759f4cadc
md mia <sharifmia091@gmail.com>
2018-08-12 05:16:13 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,7 +2,7 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
-    birth_year=1984
+    birth_year=1999
     age_of_person=Time.now.year- birth_year
 
 
